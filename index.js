a0ede9e498126d6e601398571225d20e1bbc6cac
TalonStudios <113108821+TalonStudios@users.noreply.github.com>
2026-03-23 15:44:32 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -2,23 +2,7 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
-const { MongoClient } = require("mongodb");
-// const uri = "mongodb+srv://connor:knight1@cknight355.p42a8c0.mongodb.net/?appName=cknight355";
-const uri = "mongodb://connor:knight1@ac-qzfmanw-shard-00-00.p42a8c0.mongodb.net:27017,ac-qzfmanw-shard-00-01.p42a8c0.mongodb.net:27017,ac-qzfmanw-shard-00-02.p42a8c0.mongodb.net:27017/?ssl=true&replicaSet=atlas-akyzff-shard-0&authSource=admin&appName=cknight355";
-let client = new MongoClient(uri);
-
-async function main() {
-    try {
-        await client.connect();
-        console.log("MongoDB connection successfully established.");
-        
-    } catch (e) {
-        console.log(e);
-    }
-}
-
-const server = http.createServer(async (req, res) => {
-
+const server = http.createServer((req, res) => {
     //Homepage
     if (req.url === "/") {
         fs.readFile(
@@ -37,30 +21,19 @@ const server = http.createServer(async (req, res) => {
     
     //For homepage/api
     else if (req.url === "/api") {
-        // fs.readFile(
-        //     path.join(__dirname, "public", "db.json"),
-        //     "utf-8",
-        //     (err, content) => {
-        //         if (err) {
-        //             res.writeHead(500);
-        //             return res.end("Error loading db.json");
-        //         }
+        fs.readFile(
+            path.join(__dirname, "public", "db.json"),
+            "utf-8",
+            (err, content) => {
+                if (err) {
+                    res.writeHead(500);
+                    return res.end("Error loading db.json");
+                }
 
-        //         res.writeHead(200, { "Content-Type": "application/json" });
-        //         res.end(content);
-        //     }
-        // );
-        try {
-            const data = await client.db("games_database").collection("game_collection").find({}).toArray();
-
-            res.writeHead(200, { "Content-Type": "application/json" });
-            res.end(JSON.stringify(data));
-
-        } catch (e) {
-            res.writeHead(500);
-            res.end("Database error");
-            console.error(e);
-        }
+                res.writeHead(200, { "Content-Type": "application/json" });
+                res.end(content);
+            }
+        );
     }
 
     //Give a 404 error for anything else
@@ -69,8 +42,6 @@ const server = http.createServer(async (req, res) => {
         res.end("<h1>404 - Page Not Found</h1>");
     }
 });
-
-main();
 
 //Runs the server on this port
 const PORT= process.env.PORT || 3000;
