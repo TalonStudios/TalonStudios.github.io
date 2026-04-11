e623d29e4f4c42fd241604d195de1306caf88235
TalonStudios <113108821+TalonStudios@users.noreply.github.com>
2026-04-11 13:28:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,28 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
+const {MongoClient} = require("mongodb");
+require("dotenv").config();
+const uri = process.env.MONGO_URI;
+
+
+const client = new MongoClient(uri);
+
+let gamesCollection;
+async function connectDB() {
+    try{
+        await client.connect();
+        gamesCollection = client.db("games_database").collection("game_collection");
+        console.log("Connected to MongoDB. . .");
+    }catch (error) {
+        console.error(
+            "MongoDB connection failed. . ?: ", error
+        );
+        //Exit from the entire program
+        process.exit(1);
+    }
+}
+
 const server = http.createServer((req, res) => {
     //Homepage
     if (req.url === "/") {
@@ -21,19 +43,15 @@ const server = http.createServer((req, res) => {
     
     //For homepage/api
     else if (req.url === "/api") {
-        fs.readFile(
-            path.join(__dirname, "public", "db.json"),
-            "utf-8",
-            (err, content) => {
-                if (err) {
-                    res.writeHead(500);
-                    return res.end("Error loading db.json");
-                }
-
-                res.writeHead(200, { "Content-Type": "application/json" });
-                res.end(content);
+        gamesCollection.find({}).toArray().then(
+            results => {
+                res.writeHead(200, {'Content-Type': 'application/json'});
+                res.end(JSON.stringify(results));
             }
-        );
+        ).catch(error => {
+            res.writeHead(500, {'Content-Type': 'application/json'});
+            res.end(JSON.stringify({error: "Failed to fetch the games."}));
+        })
     }
 
     //Give a 404 error for anything else
@@ -44,8 +62,10 @@ const server = http.createServer((req, res) => {
 });
 
 //Runs the server on this port
-const PORT= process.env.PORT || 3000;
+const PORT= 3000;
 // port, callback
-server.listen(PORT,()=> {
-    console.log(`server running on port ${PORT} `)
-});
+connectDB().then(
+    ()=> {
+        server.listen(PORT, ()=> console.log("Server running. . ."));
+    }
+);
